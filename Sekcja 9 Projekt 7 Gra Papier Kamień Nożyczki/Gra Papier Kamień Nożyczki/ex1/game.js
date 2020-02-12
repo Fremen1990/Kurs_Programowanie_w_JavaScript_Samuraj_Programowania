8527419616be0aa957998682195bd651bc208bc5
Tomasz Stanisz <fremen1990@gmail.com>
2020-02-13 00:06:24 +0100

    "Sekcja 9 Projekt 7 Gra Papier Kamień Nożyczki v1"

--- Sekcja 9 Projekt 7 Gra Papier Kamień Nożyczki/Gra Papier Kamień Nożyczki/ex1/game.js
+++ Sekcja 9 Projekt 7 Gra Papier Kamień Nożyczki/Gra Papier Kamień Nożyczki/ex1/game.js
@@ -25,16 +25,59 @@ function handSelection() {
 }
 
 function aiChoice() {
-    return hands[Math.floor(Math * random() * 3)].dataset.option;
+    return hands[Math.floor(Math.random() * 3)].dataset.option;
 
 }
 
+function checkResult(player, ai) {
+    if (player === ai) {
+        console.log("remis")
+        return "draw"
+    } else if ((player === "papier" && ai === "kamień") || (player === "kamień" && ai === "nożyczki") || (player === "nożyczki" && ai === "papier")) {
+        console.log("wygrałeś!!");
+        return "win"
+    } else {
+        console.log("przegrałeś!")
+        return "loss"
+    }
+}
+//Publikacja wyniku
+function publishResult(player, ai, result) {
+    document.querySelector('[data-summary="your-choice"]').textContent = player
+    document.querySelector('[data-summary="ai-choice"]').textContent = ai
+    document.querySelector(".numbers span").textContent = ++gameSummary.numbers
+
+    if (result === "win") {
+        document.querySelector(".wins span").textContent = ++gameSummary.wins;
+        document.querySelector('[data-summary="who-win"]').textContent = "TY WYGRAŁEŚ!!! ;)"
+        document.querySelector('[data-summary="who-win"]').style.color = "green";
+    } else if (result === "loss") {
+        document.querySelector(".losses span").textContent = ++gameSummary.losses;
+        document.querySelector('[data-summary="who-win"]').textContent = "PRZEGRAŁEŚ :("
+        document.querySelector('[data-summary="who-win"]').style.color = "red";
+    } else {
+        document.querySelector('.draws span').textContent = ++gameSummary.draws;
+        document.querySelector('[data-summary="who-win"]').textContent = "REMIS ://"
+        document.querySelector('[data-summary="who-win"]').style.color = "orange"
+    }
+}
+
+function endGame() {
+    document.querySelector(`[data-option="${game.playerHand}"]`).style.boxShadow = "";
+    game.playerHand = "";
+    aiHand = "";
+}
 // funkcja steruująca
 function startGame() {
     if (!game.playerHand) {
         return alert("wybierz dłoń!!!")
     }
-    game.aiHand = computerChoice()
+    game.aiHand = aiChoice()
+    console.log(game.aiHand)
+    const gameResult = checkResult(game.playerHand, game.aiHand)
+    console.log(gameResult);
+    publishResult(game.playerHand, game.aiHand, gameResult)
+    endGame()
 }
 
 //3333333333333333333333333333333333333333333333333333333333
