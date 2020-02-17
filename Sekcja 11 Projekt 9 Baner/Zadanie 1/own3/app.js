2fd8d15a081a6d0771b0a3b7a555d58804ec4123
Tomasz Stanisz <fremen1990@gmail.com>
2020-02-17 01:05:22 +0100

    Sekcja 11 Projekt 9 Baner Zadanie 3 v1

--- Sekcja 11 Projekt 9 Baner/Zadanie 1/own3/app.js
+++ Sekcja 11 Projekt 9 Baner/Zadanie 1/own3/app.js
@@ -13,7 +13,7 @@ const slide = document.querySelector("img.slider");
 const h1 = document.querySelector("h1.slider");
 const dots = [...document.querySelectorAll(".dots span")];
 
-const time = 1000;
+const time = 2000;
 let activeOne = 0;
 
 const changeSlide = () => {
@@ -33,3 +33,29 @@ const changeDots = () => {
 }
 
 setInterval(changeSlide, time)
+
+dots.forEach(dot => dot.addEventListener("click", (e) => {
+    console.log(e.target.id)
+    const target = e.target.id
+    if (target === "one") {
+        slide.src = sliders[0].img;
+        h1.textContent = sliders[0].txt;
+        const activeDots = dots.findIndex(dot => dot.classList.contains("active"));
+        dots[activeDots].classList.remove("active");
+        dots[0].classList.add("active")
+    }
+    if (target === "two") {
+        slide.src = sliders[1].img;
+        h1.textContent = sliders[1].txt;
+        const activeDots = dots.findIndex(dot => dot.classList.contains("active"));
+        dots[activeDots].classList.remove("active");
+        dots[1].classList.add("active")
+    }
+    if (target === "three") {
+        slide.src = sliders[2].img;
+        h1.textContent = sliders[2].txt;
+        const activeDots = dots.findIndex(dot => dot.classList.contains("active"));
+        dots[activeDots].classList.remove("active");
+        dots[2].classList.add("active")
+    }
+}))
