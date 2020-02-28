3bdf60828c80e084a3596b00440fe4b6d6a3d327
Tomasz Stanisz <fremen1990@gmail.com>
2020-02-28 02:34:33 +0100

    Sekcja 16 OOP PROJEKT Jednoręki bandyta JS CLASS DRAW

---
+++ Sekcja 16 OOP PROJEKT/GAME/Draw.js
@@ -0,0 +1,19 @@
+class Draw {
+    constructor() {
+        this.options = ["red", "green", "blue"]; // this.options przechowuje opcje kolorów jakie są do wylosowania
+        let _result = this.drawResult(); // let _result przechowuje nam tablice z wynikiem
+        this.getDrawResult = () => _result;
+    }
+    drawResult() {
+        let colors = [];
+        /// uzupełnianie poprzez losowanie
+        for (let i = 0; i < this.options.length; i++) { //  losowanie przy użyciu pętli for
+            const index = Math.floor(Math.random() * this.options.length); // określenie indexu poprzez ranodom * ilość wyborów , index porzebny do wyboru z tablicy this.options   
+            const color = this.options[index];
+            console.log(color);
+            colors.push(color);
+        }
+        return colors
+    }
+}
+// const draw = new Draw();
--- Sekcja 16 OOP PROJEKT/GAME/Statistics.js
+++ Sekcja 16 OOP PROJEKT/GAME/Statistics.js
@@ -19,7 +19,6 @@ class Statistics {
         console.log(gameResult)
         this.gameResults.push(gameResult);
     }
-
     showGameStatistics() {
         let games = this.gameResults.length
         let wins = this.gameResults.filter(result => result.win).length;
@@ -28,4 +27,4 @@ class Statistics {
         return [games, wins, losses]
     }
 }
-const stats = new Statistics()
+// const stats = new Statistics()
