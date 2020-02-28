42f92b4c14dec4a39e4c64732520ba1b769b93f6
Tomasz Stanisz <fremen1990@gmail.com>
2020-02-28 02:20:23 +0100

    Sekcja 16 OOP PROJEKT Jednoręki bandyta JS CLASS STATISTICS

--- Sekcja 16 OOP PROJEKT/GAME/Wallet.js
+++ Sekcja 16 OOP PROJEKT/GAME/Wallet.js
@@ -3,20 +3,25 @@ class Wallet {
         let _money = money;
         //pobieranie aktuwalnej zawartości portfela
         this.getWalletValue = () => _money;
-
         //Sprawdzanie czy użytkownik ma odpowiednią ilośc środków
-
         this.checlCanPlay = (value) => {
             if (_money >= value) return true;
             return false
         }
-
         this.changeWallet = (value, type = "+") => {
-
+            if (typeof value === "number" && !isNaN(value)) { // sprawdzenie czy value type jest liczba i czy nie jest not a number (negacja z true isNAN)
+                if (type === "+") {
+                    return _money += value;
+                } else if (type === "-") {
+                    return _money -= value;
+                } else {
+                    throw new Error("nieprawidłowy typ działania")
+                }
+            } else {
+                console.log(typeof value);
+                throw new Error("nieprawidłowa liczba")
+            }
         }
-
-
     }
 }
-
-const wallet = new Wallet(200)
+// const wallet = new Wallet(200)  //obiekt zostanie stworzony w pliku  Game.js który będzie spajał klasy ze wszystkich modułów/plików
